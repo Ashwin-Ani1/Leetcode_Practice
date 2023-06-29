4006ec8fbf1eafa325f24c09057ecc7f89d72d90
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2023-06-29 15:57:20 -0400

    Time: 489 ms (68.25%), Space: 29.2 MB (39.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def containsDuplicate(self, nums):
+        """
         #first sorted the array in order because I want to compare the values side by side
         nums.sort()
         #Had to do len(nums)-1 since I will get an error for a greater length
@@ -7,3 +8,17 @@ class Solution(object):
         for i in range (len(nums)-1):
             if nums[i] == nums[i + 1]:
                 return True
+        #time O(nlogn) due to sorting and space O(1)    
+        """    
+            
+        #"""
+        #More efficient code that is timeCO{O(n)} Space O(n) 
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
+        #"""
+        
