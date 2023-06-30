ee3b376df7a3402c201881402f9a20c52461a13a
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2023-06-30 16:09:47 -0400

    Time: 39 ms (73.64%), Space: 15.2 MB (27.26%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        char_count = [0] * 26
+    
+        for i in range(len(s)):
+            char_count[ord(s[i]) - ord('a')] += 1
+            char_count[ord(t[i]) - ord('a')] -= 1
+    
+        for count in char_count:
+            if count != 0:
+                return False
+        return True
