70624400f760665539c3b01738e316a4321f159e
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2023-09-11 21:34:29 -0400

    Time: 469 ms (32.62%), Space: 23.2 MB (91.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,7 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        
+        nums.sort()
+        for i in range (len(nums) -1):
+            if(nums[i] == nums[i+1]):
+                return True
+        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         """
+        
         nums.sort()
         #Had to do len(nums)-1 since I will get an error for a greater length
         #if at least one match there will be a return of true
@@ -12,7 +39,7 @@ class Solution(object):
             if nums[i] == nums[i + 1]:
                 return True
         #time O(nlogn) due to sorting and space O(1)    
-        """
+        
         # More efficient code that is timeCO{O(n)} Space O(n)
                 #first sorted the array in order because I want to compare the values side by side
         hashset = set()
@@ -21,3 +48,10 @@ class Solution(object):
                 return True
             hashset.add(n)
         return False
+        
+        """
+        
+
+            
+            
+            
