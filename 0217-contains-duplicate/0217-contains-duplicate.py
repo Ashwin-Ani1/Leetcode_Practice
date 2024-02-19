9094749f496769f9a998f0174e117c0c5ce6d574
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-19 13:50:06 -0500

    Time: 504 ms (12.99%), Space: 21.3 MB (98.00%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,54 +4,63 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-        """
-        hashset = set()
-        for n in nums:
-            if n in hashset:
-                return True
-            else:
-                hashset.add(n)
-        return False
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        
+       """
         nums.sort()
-        #Had to do len(nums)-1 since I will get an error for a greater length
-        #if at least one match there will be a return of true
-        for i in range (len(nums)-1):
-            if nums[i] == nums[i + 1]:
+        for n in range (len(nums)-1):
+            if nums[n] == nums[n+1]:
                 return True
-        #time O(nlogn) due to sorting and space O(1)    
+        return False 
         
-        # More efficient code that is timeCO{O(n)} Space O(n)
-                #first sorted the array in order because I want to compare the values side by side
         
-        hashset = set()
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
-        """
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+        
+        
+#         nums.sort()
+#         #Had to do len(nums)-1 since I will get an error for a greater length
+#         #if at least one match there will be a return of true
+#         for i in range (len(nums)-1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+#         #time O(nlogn) due to sorting and space O(1)    
+        
+#         # More efficient code that is timeCO{O(n)} Space O(n)
+#                 #first sorted the array in order because I want to compare the values side by side
+        
+#         hashset = set()
+#         for n in nums:
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
+#         return False
+    
+    
+        
