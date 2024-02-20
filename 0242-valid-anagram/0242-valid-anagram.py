d53197ef0c14f4b0baed5e64dbeca208203db7f6
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-20 11:21:29 -0500

    Time: 44 ms (26.85%), Space: 13.5 MB (59.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,35 +5,33 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
-#         if len(s) != len(t):
-#             return False
-#         countS, countT = {}, {}
-        
-#         for c in range(len(s)):
-#             countS[s[c]] = 1 + countS.get(s[c],0)
-#             countT[s[c]] = 1 + countT.get(t[c],0)
-#         for c in countS:
-#             if countS[c] != countT.get(c,0):
-#                 return False
-#         return True
-
-        
-        
-        
-        
-        
         if len(s) != len(t):
             return False
-        countS, countT = {}, {}
+        countS, countT = {},{}
         
-        for i in range (len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
-        for c in countS:
-            if countS[c] != countT.get(c,0): 
+        for n in range (len(s)):
+            countS[s[n]] = 1 + countS.get(s[n],0)
+            countT[t[n]] = 1 + countT.get(t[n],0)
+        for n in countS:
+            if countS[n] != countT.get(n,0):
                 return False
         return True
+
+        
+        # if len(s) != len(t):
+        #     return False
+        # #first case check if the lengths are equal
+        # countS, countT = {}, {}
+        # #set up hashmaps
+        # for i in range (len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i],0)
+        #     countT[t[i]] = 1 + countT.get(t[i],0)
+        #     #add the letters to each map 
+        # for c in countS:
+        #     if countS[c] != countT.get(c,0): 
+        #         #reference each map
+        #         return False
+        # return True
         
         
         
