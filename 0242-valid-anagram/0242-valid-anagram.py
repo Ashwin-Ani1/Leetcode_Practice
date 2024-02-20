a7f9bc34d5996bd4f72b30902149145841b1c31a
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-20 11:58:59 -0500

    Time: 45 ms (24.39%), Space: 13.6 MB (59.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,15 +7,21 @@ class Solution(object):
         """
         if len(s) != len(t):
             return False
-        countS, countT = {},{}
-        
+        mapS, mapT = {},{}
         for n in range (len(s)):
-            countS[s[n]] = 1 + countS.get(s[n],0)
-            countT[t[n]] = 1 + countT.get(t[n],0)
-        for n in countS:
-            if countS[n] != countT.get(n,0):
+            mapS[s[n]] = 1 + mapS.get(s[n],0)
+            mapT[t[n]] = 1 + mapT.get(t[n],0)
+        for n in mapS:
+            if mapS[n] != mapT.get(n,0):
                 return False
         return True
+        
+        
+        
+        
+
+        
+        
 
         
         # if len(s) != len(t):
