731e9d242bb9001c9be5f83659b3e7799c8dd217
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-21 10:27:12 -0500

    Time: 499 ms (13.75%), Space: 21.4 MB (97.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,17 +5,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
        """
-
-
-
-
         nums.sort()
-        #Had to do len(nums)-1 since I will get an error for a greater length
-        #if at least one match there will be a return of true
         for i in range (len(nums)-1):
-            if nums[i] == nums[i + 1]:
+            if nums[i] == nums[i+1]:
                 return True
-#         #time O(nlogn) due to sorting and space O(1)    
+        return False
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+#         nums.sort()
+#         #Had to do len(nums)-1 since I will get an error for a greater length
+#         #if at least one match there will be a return of true
+#         for i in range (len(nums)-1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+# #         #time O(nlogn) due to sorting and space O(1)    
         
 # #         # More efficient code that is timeCO{O(n)} Space O(n)
 # #                 #first sorted the array in order because I want to compare the values side by side
