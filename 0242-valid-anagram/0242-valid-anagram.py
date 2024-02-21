c828e66a77b235c331757b7da332623413b3acbd
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-21 11:48:13 -0500

    Time: 32 ms (70.75%), Space: 12.7 MB (77.70%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,18 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        hashs,hasht = {},{}
-        for i in range (len(s)):
-            hashs[s[i]] = 1 + hashs.get(s[i],0)
-            hasht[t[i]] = 1 + hasht.get(t[i],0)
-        for i in hashs:
-            if hashs[i] != hasht.get(i,0):
-                return False
+#         if len(s) != len(t):
+#             return False
+#         hashs,hasht = {},{}
+#         for i in range (len(s)):
+#             hashs[s[i]] = 1 + hashs.get(s[i],0)
+#             hasht[t[i]] = 1 + hasht.get(t[i],0)
+#         for i in hashs:
+#             if hashs[i] != hasht.get(i,0):
+#                 return False
             
-        return True
+#         return True
         
+        return sorted(str(s)) == sorted(str(t))
         
         
         
