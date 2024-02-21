49a544910be5913ecaa9fef7b7743a7aad2ab05e
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-21 10:24:34 -0500

    Time: 502 ms (13.29%), Space: 21.4 MB (96.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,7 +5,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
        """
-   
+
+
+
+
         nums.sort()
         #Had to do len(nums)-1 since I will get an error for a greater length
         #if at least one match there will be a return of true
@@ -14,15 +17,15 @@ class Solution(object):
                 return True
 #         #time O(nlogn) due to sorting and space O(1)    
         
-#         # More efficient code that is timeCO{O(n)} Space O(n)
-#                 #first sorted the array in order because I want to compare the values side by side
+# #         # More efficient code that is timeCO{O(n)} Space O(n)
+# #                 #first sorted the array in order because I want to compare the values side by side
         
-#         hashset = set()
-#         for n in nums:
-#             if n in hashset:
-#                 return True
-#             hashset.add(n)
-#         return False
+# #         hashset = set()
+# #         for n in nums:
+# #             if n in hashset:
+# #                 return True
+# #             hashset.add(n)
+# #         return False
     
     
         
