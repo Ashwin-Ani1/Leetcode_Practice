2da8d45a799f26cf800f0e11a2d940befda6e678
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-23 16:16:38 -0500

    Time: 466 ms (22.61%), Space: 21.4 MB (97.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,16 +4,12 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: bool
-       """
-        hashsetS = set()
-        for n in nums:
-            if n in hashsetS:
+       """ 
+        nums.sort()
+        for i in range (len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return True
-            else:
-                hashsetS.add(n)
-                
         return False
-    
         
         
         
@@ -30,24 +26,28 @@ class Solution(object):
         
         
         
-
-#         nums.sort()
-#         #Had to do len(nums)-1 since I will get an error for a greater length
-#         #if at least one match there will be a return of true
-#         for i in range (len(nums)-1):
-#             if nums[i] == nums[i + 1]:
+        
+        
+        
+        
+        
+        nums.sort()
+        #Had to do len(nums)-1 since I will get an error for a greater length
+        #if at least one match there will be a return of true
+        for i in range (len(nums)-1):
+            if nums[i] == nums[i + 1]:
+                return True
+#         #time O(nlogn) due to sorting and space O(1)    
+        
+#         # More efficient code that is timeCO{O(n)} Space O(n)
+#                 #first sorted the array in order because I want to compare the values side by side
+        
+#         hashset = set()
+#         for n in nums:       
+#             if n in hashset:
 #                 return True
-# #         #time O(nlogn) due to sorting and space O(1)    
-        
-# #         # More efficient code that is timeCO{O(n)} Space O(n)
-# #                 #first sorted the array in order because I want to compare the values side by side
-        
-# #         hashset = set()
-# #         for n in nums:
-# #             if n in hashset:
-# #                 return True
-# #             hashset.add(n)
-# #         return False
+#             hashset.add(n)
+#         return False
     
     
         
