8dd169aabf5ae983019df989e374c5ad3dbf80d3
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-23 18:04:13 -0500

    Time: 34 ms (90.87%), Space: 12.6 MB (56.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,24 +4,49 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """
+        """ 
         
-#         hashMap = {}
-#         for i, n in enumerate(nums):
-#             diff = target - n
-#             if diff in hashMap:
-#                 return [hashMap[diff],i]
-#             hashMap[n] = i
-#         return 
+        hashMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff],i]
+            hashMap[n] = i
+        return 
         
         
-        #My solution
-        n=0 
-        while n != len(nums): #loop through the list
-            i = n + 1 #i represents the next value of n 
-            while i != (len(nums)): 
-                if nums[n] + nums[i] == target:
-                    return [n,i] #Nested loop so its o(n^2) complexity
-                i += 1 
-            n+=1                                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # hashMap = {}
+        # for i, n in enumerate(nums):
+        #     diff = target - n
+        #     if diff in hashMap:
+        #         return [hashMap[diff],i]
+        #     hashMap[n] = i
+        # return 
+        
+        
+        # #My solution
+        # n=0 
+        # while n != len(nums): #loop through the list
+        #     i = n + 1 #i represents the next value of n 
+        #     while i != (len(nums)): 
+        #         if nums[n] + nums[i] == target:
+        #             return [n,i] #Nested loop so its o(n^2) complexity
+        #         i += 1 
+        #     n+=1                                
         
