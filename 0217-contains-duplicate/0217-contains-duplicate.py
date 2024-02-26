e57494441e78fc4fee836f89be4184f4a9d69765
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-26 09:07:32 -0500

    Time: 475 ms (19.82%), Space: 21.3 MB (99.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,41 +5,47 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
        """ 
-        hashS = set()
-        for n in nums:
-            if n in hashS:
-                return True
-            else:
-                hashS.add(n)
-        return False
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         nums.sort()
-        #Had to do len(nums)-1 since I will get an error for a greater length
-        #if at least one match there will be a return of true
-        for i in range (len(nums)-1):
-            if nums[i] == nums[i + 1]:
+        for n in range (len(nums)-1):
+            if nums[n] == nums[n+1]:
                 return True
+        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # nums.sort()
+        # #Had to do len(nums)-1 since I will get an error for a greater length
+        # #if at least one match there will be a return of true
+        # for i in range (len(nums)-1):
+        #     if nums[i] == nums[i + 1]:
+        #         return True
 #         #time O(nlogn) due to sorting and space O(1)    
         
 #         # More efficient code that is timeCO{O(n)} Space O(n)
