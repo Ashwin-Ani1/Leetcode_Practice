02c90a2d0bc1cf2fc77cf0942525d70cabbeb971
Ashwin-Ani1 <103693728+Ashwin-Ani1@users.noreply.github.com>
2024-02-26 11:51:41 -0500

    Time: 32 ms (58.86%), Space: 12.3 MB (90.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s):
-        """
+        
         l, r = 0, len(s)-1
         while l<r:
             while l<r and not s[l].isalnum():
@@ -12,11 +12,22 @@ class Solution:
             l+=1
             r-=1
         return True
-        """
-        Str = ""
-        for c in s:
-            if c.isalnum():
-                Str += c.lower()
-        return Str == Str[::-1]
+        
+            
+        
+        
+        
+        
+        # #First initialize String    
+        # Str = ""
+        # #Create a new string with no spaces
+        # for c in s:
+        #     if c.isalnum():
+        #         #make sure all alpha is lowercase
+        #         Str += c.lower()
+        # #compare the string and its reversed string
+        # return Str == Str[::-1]
+        # #Time O(n): loop through a string(n time)
+        # #Space O(n): create a new string so memory being allocated
 
         
